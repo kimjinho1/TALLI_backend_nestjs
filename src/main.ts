9337b167c6bd7544763a1f4243f77fbc99dc5f52
kimjinho1 <rlawlsgh8113@naver.com>
2023-05-29 07:33:16 +0900

    feat: swagger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 /*
 TODO
@@ -29,7 +30,17 @@ TODO
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
-  console.log('http://localhost:3000')
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
+
+console.log('http://localhost:3000')
 bootstrap()
