632caa9f0e317144d2a1122ae5e48f1b088c02c6
kimjinho1 <rlawlsgh8113@naver.com>
2023-06-02 16:55:29 +0900

    feat: 회사 정보 삭제 api 구현

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -31,10 +31,25 @@ export class CompanyService {
       throw new NotFoundException('존재하지 않는 회사입니다')
     }
 
-    // Company 생성
+    // Company 업데이트
     const updatedCompany: Company = await this.repository.updateCompany(companyId, updateCompanyDto)
 
     // post 결과 반환
     return updatedCompany
+  }
+
+  // 회사 정보 수정
+  async deleteCompany(companyId: number): Promise<Company> {
+    // 존재하는 회사인지 확인 -> 에러일 시 404 에러 코드 반환
+    const existedCompany: Company | null = await this.repository.getCompanyById(companyId)
+    if (!existedCompany) {
+      throw new NotFoundException('존재하지 않는 회사입니다')
+    }
+
+    // Company 삭제
+    const deletedCompany: Company = await this.repository.deleteCompany(companyId)
+
+    // post 결과 반환
+    return deletedCompany
   }
 }
