f45a1deb65e4d55c994273243eb4d0500f98a22e
kimjinho1 <rlawlsgh8113@naver.com>
2023-06-02 16:43:39 +0900

    feat: getCompanyById 구현

--- src/company/repository/company.repository.ts
+++ src/company/repository/company.repository.ts
@@ -2,10 +2,20 @@ import { Injectable } from '@nestjs/common'
 import { Company } from '@prisma/client'
 import { PrismaService } from 'prisma/prisma.service'
 import { CreateCompanyDto } from '../dto/CreateCompany.dto'
+import { UpdateCompanyDto } from '../dto/UpdateCompany.dto'
 
 @Injectable()
 export class CompanyRepository {
   constructor(private prisma: PrismaService) {}
+
+  // companyId로 Company 찾기
+  async getCompanyById(companyId: number): Promise<Company | null> {
+    return await this.prisma.company.findUnique({
+      where: {
+        companyId
+      }
+    })
+  }
 
   // companyName으로 Company 찾기
   async getCompanyByCompanyName(companyName: string): Promise<Company | null> {
@@ -21,6 +31,18 @@ export class CompanyRepository {
     return await this.prisma.company.create({
       data: {
         ...createCompanyDto
+      }
+    })
+  }
+
+  // 회사 정보 수정
+  async updateCompany(companyId: number, updateCompanyDto: UpdateCompanyDto): Promise<Company> {
+    return await this.prisma.company.update({
+      where: {
+        companyId
+      },
+      data: {
+        ...updateCompanyDto
       }
     })
   }
