381b317f725e85c22324adb4d114e9ab2058bb2b
kimjinho1 <rlawlsgh8113@naver.com>
2023-06-02 16:44:46 +0900

    feat: 회사 정보 수정 api 구현

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common'
 import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { Company } from '@prisma/client'
 import { CompanyService } from './company.service'
 import { CreateCompanyDto } from './dto/CreateCompany.dto'
+import { UpdateCompanyDto } from './dto/UpdateCompany.dto'
 
 @Controller('company')
 @ApiTags('회사 API')
@@ -14,5 +15,15 @@ export class CompanyController {
   @ApiOperation({ summary: '회사 정보 추가' })
   async createCompany(@Body() createCompanyDto: CreateCompanyDto): Promise<Company> {
     return await this.companyService.createCompany(createCompanyDto)
+  }
+
+  @Patch('/:companyId')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: '회사 정보 수정' })
+  async UpdateCompany(
+    @Param('companyId') companyId: number,
+    @Body() updateCompanyDto: UpdateCompanyDto
+  ): Promise<Company> {
+    return await this.companyService.updateCompany(companyId, updateCompanyDto)
   }
 }
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,6 +1,7 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common'
 import { Company } from '@prisma/client'
 import { CreateCompanyDto } from './dto/CreateCompany.dto'
+import { UpdateCompanyDto } from './dto/UpdateCompany.dto'
 import { CompanyRepository } from './repository/company.repository'
 
 @Injectable()
@@ -12,7 +13,7 @@ export class CompanyService {
     // 회사 이름 중복 처리 -> 에러일 시 409 에러 코드 반환
     const existedCompany: Company | null = await this.repository.getCompanyByCompanyName(createCompanyDto.companyName)
     if (existedCompany) {
-      throw new HttpException('이미 존재하는 회사 이름입니다.', HttpStatus.CONFLICT)
+      throw new ConflictException('이미 존재하는 회사 이름입니다.')
     }
 
     // Company 생성
@@ -20,5 +21,20 @@ export class CompanyService {
 
     // post 결과 반환
     return createdCompany
+  }
+
+  // 회사 정보 수정
+  async updateCompany(companyId: number, updateCompanyDto: UpdateCompanyDto): Promise<Company> {
+    // 존재하는 회사인지 확인 -> 에러일 시 404 에러 코드 반환
+    const existedCompany: Company | null = await this.repository.getCompanyById(companyId)
+    if (!existedCompany) {
+      throw new NotFoundException('존재하지 않는 회사입니다')
+    }
+
+    // Company 생성
+    const updatedCompany: Company = await this.repository.updateCompany(companyId, updateCompanyDto)
+
+    // post 결과 반환
+    return updatedCompany
   }
 }
