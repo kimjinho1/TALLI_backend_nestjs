6b10f1dd3448d50ffd59c0f3edfae6837fd0f2fa
kimjinho1 <rlawlsgh8113@naver.com>
2023-06-04 05:37:10 +0900

    chore: lint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-explicit-any': 'warning',
     'prettier/prettier': [
       'error',
       {
