393955ecee7262a3feeddc8fe10e418573d25b9c
kimjinho1 <rlawlsgh8113@naver.com>
2023-06-14 00:50:49 +0900

    feat: 회사 관련 api 스웨거 작성

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,18 +1,36 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common'
-import { ApiOperation, ApiTags } from '@nestjs/swagger'
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiConflictResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiTags
+} from '@nestjs/swagger'
 import { Company } from '@prisma/client'
 import { CompanyService } from './company.service'
 import { CreateCompanyRequestDto, UpdateCompanyRequestDto } from './dto/request'
 import { GetCompanyListResponseDto } from './dto/response'
 
-@Controller('company')
 @ApiTags('회사 API')
+@Controller('company')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
+  @ApiOperation({ summary: '전체 회사 정보 보기', description: '모든 회사의 정보를 반환합니다.' })
+  @ApiQuery({ name: 'index', type: Number })
+  @ApiQuery({ name: 'difference', type: Number })
+  @ApiOkResponse({
+    description: '성공 시, 200 Ok를 응답합니다.',
+    type: GetCompanyListResponseDto
+  })
+  @ApiBadRequestResponse({
+    description: '쿼리(index, difference)가 올바르지 않은 경우, 400 Bad Request 응답합니다.'
+  })
   @Get('/list')
   @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: '전체 회사 정보 보기' })
   async getCompanyList(
     @Query('index') index: number,
     @Query('difference') difference: number
@@ -20,23 +38,45 @@ export class CompanyController {
     return await this.companyService.getCompanyList(index, difference)
   }
 
+  @ApiOperation({ summary: '개별 회사 정보 보기', description: 'companyId에 매칭되는 회사의 정보를 반환합니다.' })
+  @ApiQuery({ name: 'companyId', type: Number })
+  @ApiOkResponse({
+    description: '성공 시, 200 Ok를 응답합니다.'
+  })
+  @ApiNotFoundResponse({
+    description: 'companyId에 매칭되는 회사가 없는 경우, 404 Not Found 응답합니다.'
+  })
   @Get('/:companyId')
   @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: '개별 회사 정보 보기' })
   async getCompanyById(@Param('companyId') companyId: number): Promise<Company> {
     return await this.companyService.getCompanyById(companyId)
   }
 
+  @ApiOperation({ summary: '회사 정보 추가', description: '회사 정보를 추가합니다.' })
+  @ApiBody({ type: CreateCompanyRequestDto })
+  @ApiOkResponse({
+    description: '성공 시, 200 Ok를 응답합니다.'
+  })
+  @ApiConflictResponse({
+    description: '회사 이름이 중복인 경우, 409 Conflict 응답합니다.'
+  })
   @Post()
   @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: '회사 정보 추가' })
   async createCompany(@Body() createCompanyDto: CreateCompanyRequestDto): Promise<Company> {
     return await this.companyService.createCompany(createCompanyDto)
   }
 
+  @ApiOperation({ summary: '회사 정보 수정', description: 'companyId에 매칭되는 회사의 정보를 수정합니다.' })
+  @ApiQuery({ name: 'companyId', type: Number })
+  @ApiBody({ type: UpdateCompanyRequestDto })
+  @ApiOkResponse({
+    description: '성공 시, 200 Ok를 응답합니다.'
+  })
+  @ApiNotFoundResponse({
+    description: 'companyId에 매칭되는 회사가 없는 경우, 404 Not Found 응답합니다.'
+  })
   @Patch('/:companyId')
   @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: '회사 정보 수정' })
   async updateCompany(
     @Param('companyId') companyId: number,
     @Body() updateCompanyDto: UpdateCompanyRequestDto
@@ -44,9 +84,16 @@ export class CompanyController {
     return await this.companyService.updateCompany(companyId, updateCompanyDto)
   }
 
+  @ApiOperation({ summary: '회사 정보 삭제', description: 'companyId에 매칭되는 회사의 정보를 삭제합니다.' })
+  @ApiQuery({ name: 'companyId', type: Number })
+  @ApiOkResponse({
+    description: '성공 시, 200 Ok를 응답합니다.'
+  })
+  @ApiNotFoundResponse({
+    description: 'companyId에 매칭되는 회사가 없는 경우, 404 Not Found 응답합니다.'
+  })
   @Delete('/:companyId')
   @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: '회사 정보 삭제' })
   async deleteCompany(@Param('companyId') companyId: number): Promise<Company> {
     return await this.companyService.deleteCompany(companyId)
   }
--- src/company/dto/response/get.company.list.response.dto.ts
+++ src/company/dto/response/get.company.list.response.dto.ts
@@ -1,6 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Company } from '@prisma/client'
 
 export class GetCompanyListResponseDto {
+  @ApiProperty({ description: '전체 회사 개수', example: 180 })
   numTotal: number
+
+  @ApiProperty({ description: 'Company[]' })
   resultList: Company[]
 }
