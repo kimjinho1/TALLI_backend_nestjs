86568120ca8da1fb934166fb25c115767942ce4f
kimjinho1 <rlawlsgh8113@naver.com>
2023-08-11 23:36:44 +0900

    chore: 에러 메시지 수정

--- src/common/exception/error.messages.ts
+++ src/common/exception/error.messages.ts
@@ -3,8 +3,9 @@ export const ErrorMessages = {
   CURRENT_JOB_DETAIL_NOT_FOUND: '존재하지 않는 유저 상제 정보입니다.',
   NICKNAME_OR_EMAIL_ALREADY_EXISTS: '이미 존재하는 닉네임 또는 이메일입니다.',
   NICKNAME_ALREADY_EXISTS: '이미 존재하는 닉네임입니다.',
-  INVALID_SEARCH_RANGE_INPUT: '잘못된 검색 범위 입력입니다',
-  COMPANY_NOT_FOUND: '존재하지 않는 회사입니다',
-  COMPANY_NAME_ALREADY_EXISTS: '이미 존재하는 회사 이름입니다',
-  JOB_NOTICE_ALREADY_EXISTS: '이미 존재하는 채용 공고입니다'
+  INVALID_SEARCH_RANGE_INPUT: '잘못된 검색 범위 입력입니다.',
+  COMPANY_NOT_FOUND: '존재하지 않는 회사입니다.',
+  COMPANY_NAME_ALREADY_EXISTS: '이미 존재하는 회사 이름입니다.',
+  JOB_NOTICE_NOT_FOUND: '존재하지 않는 채용 공고입니다.',
+  JOB_NOTICE_ALREADY_EXISTS: '이미 존재하는 채용 공고입니다.'
 }
