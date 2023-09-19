62d0df8d0c73d57d30eea2fcf3d7cc0720178f6b
kimjinho1 <rlawlsgh8113@naver.com>
2023-09-19 23:30:59 +0900

    feat: ServeStaticModule 삭제 & BigQueryModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
-import { ServeStaticModule } from '@nestjs/serve-static'
 import { CoreModule } from './core/core.module'
-import { StorageModule } from './storage/storage.module';
+import { StorageModule } from './storage/storage.module'
+import { BigQueryModule } from './big-query/big-query.module'
 
 @Module({
-  imports: [
-    CoreModule,
-    ServeStaticModule.forRoot({
-      rootPath: process.cwd()
-    }),
-    StorageModule
-  ],
+  imports: [CoreModule, StorageModule, BigQueryModule],
   controllers: [AppController]
 })
 export class AppModule {}
