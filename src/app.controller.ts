b0893f5542c88394e687a4e3b347d725ff18b24e
kimjinho1 <rlawlsgh8113@naver.com>
2023-09-28 01:24:44 +0900

    refact: 이미지 업로드 코드 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,26 @@
-import { BadRequestException, Controller, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
+import {
+  BadRequestException,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors
+} from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { diskStorage } from 'multer'
 import * as path from 'path'
 import * as fs from 'fs'
+import { v4 as uuidv4 } from 'uuid'
+import { StorageService } from './storage/storage.service'
 
 @Controller()
 export class AppController {
+  constructor(private readonly storageService: StorageService) {}
+
   @Get()
   getHello(): string {
     return 'Talli Backend'
@@ -14,32 +29,35 @@ export class AppController {
   @Post('/image/:path')
   @UseInterceptors(
     FileInterceptor('image', {
-      storage: diskStorage({
-        destination: (req, file, cb) => {
-          const uploadPath = path.join(process.cwd(), 'images', req.params.path)
-          cb(null, uploadPath)
-        },
-        filename: (req, file, cb) => {
-          const uploadPath = path.join(process.cwd(), 'images', req.params.path)
-          console.log(uploadPath)
-          fs.readdir(uploadPath, (err, files) => {
-            if (err) {
-              console.log(err)
-            } else {
-              const filename = `${files.length}${path.extname(file.originalname)}`
-              cb(null, filename)
-            }
-          })
-        }
-      })
+      limits: {
+        files: 1,
+        fileSize: 1024 * 1024
+      }
     })
   )
-  async imageUpload(@Param('path') path: string, @UploadedFile() file: Express.Multer.File) {
+  async imageUpload(
+    @Param('path') path: string,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new MaxFileSizeValidator({ maxSize: 1024 * 1024 }), new FileTypeValidator({ fileType: 'image/*' })]
+      })
+    )
+    file: Express.Multer.File
+  ) {
     const paths = ['job-notice', 'company', 'career', 'user']
     if (paths.includes(path) === false) {
       throw new BadRequestException('잘못된 저장 경로입니다.')
     }
-    const imageUrl = `${process.env.BACKEND_URL}/images/${path}/${file.filename}`
-    return imageUrl
+
+    const uuid = uuidv4()
+    const imagePath = `${path}/${uuid}.png`
+
+    await this.storageService.save(imagePath, file.mimetype, file.buffer, [{ imageId: imagePath }])
+
+    const result = {
+      imageUrl: imagePath
+    }
+
+    return result
   }
 }
