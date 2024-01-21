7c597660b84a98471730dc9c9a18404f1667d809
kimjinho1 <rlawlsgh8113@naver.com>
2024-01-22 03:36:20 +0900

    chore: 오타 수정

--- src/core/adapter/web/command/user/update-job-of-interest.command.ts
+++ src/core/adapter/web/command/user/update-job-of-interest.command.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger'
 import { IsDefined, IsIn } from 'class-validator'
 import { jobList } from 'src/core/application/service/dto/user/response'
 
-export class updateJobOfInterestCommand {
+export class UpdateJobOfInterestCommand {
   @ApiProperty({ type: [String], description: '최대 10개인 문자열 배열', example: ['보건관리자', '임상연구'] })
   @IsDefined()
   @IsIn(jobList, { each: true })
